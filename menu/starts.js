c1c172b3ee3ac8fec8b6169928cdb0935a82dd0e
Carol <27028067+HDNonni@users.noreply.github.com>
2018-07-14 15:11:00 -0400

    First Restaurant Site Commit

---
+++ menu/starts.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    var sweetStarts = {
+        name: "Blueberry Bites",
+        decscription: "Plump, sweet blueberries wrapped in chocolate deliciousness",
+        price: "$2"
+    }
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+});
