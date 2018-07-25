2cfbf14afb443d99c6493337707e8a5db3cb3c12
Carol <27028067+HDNonni@users.noreply.github.com>
2018-07-25 10:31:09 -0400

    updateToPages and add RichRewards

---
+++ menu/richRewards.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+ //console.log('ready')
+
+$('.clicToShow').click(function(){
+    $('.hidePic').fadeToggle("3000", function(){
+        
+    })
+})
+
+
+
+
+
+
+
+});
+
