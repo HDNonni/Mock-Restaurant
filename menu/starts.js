53c47af7ca771fa333fcb993d7a100741b8eb753
Carol <27028067+HDNonni@users.noreply.github.com>
2018-11-16 06:30:17 -0500

    cleaned up some code and changed if you must page for readability

--- menu/starts.js
+++ menu/starts.js
@@ -90,7 +90,7 @@ $(document).ready(function () {
     //url: have to use browser sync
     //const url = 'http://localhost:3000/Restaurant/menu/starts.json';
     //change to a relative url so it can be viewed
-    const url = "../menu/starts.json";
+    const url = "starts.json";
     //ajax call to url , what kind of data type and GET method
     $.ajax(url, {
         dataType: 'json',
